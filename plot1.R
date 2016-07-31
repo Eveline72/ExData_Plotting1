27537bd6272a9848782fb891408d47b35993104c
Eveline72 <eveline@evadvies.nl>
2016-07-31 10:38:05 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,24 @@
+## Read the data
+
+hhdataFile <- "household_power_consumption.txt"
+hhdata <- read.table(hhdataFile, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+
+## Take the subset of only 1/2/2007 and 2/2/2007
+
+hhdata_used <- hhdata[hhdata$Date %in% c("1/2/2007","2/2/2007"), ]
+
+## Give data the proper type (numeric)
+
+Glob_act_pow <- as.numeric(hhdata_used$Global_active_power)
+
+## Define the plot as a png of weight and height 480
+
+png("plot1.png",width = 480, height = 480)
+
+## Make the correct plot
+
+hist(Glob_act_pow, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+
+## Close off the call to png
+
+dev.off()
