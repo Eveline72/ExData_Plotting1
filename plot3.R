48d76d53f97ac5ca86d875e2180744898b00f9bf
Eveline72 <eveline@evadvies.nl>
2016-07-31 10:50:14 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -23,7 +23,7 @@ png("plot3.png",width = 480, height = 480)
 plot(dt, subM1, type = "l", ylab = "Energy Submetering", xlab = "")
 lines(dt, subM2, type = "l", col = "red")
 lines(dt, subM3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = 1, lwd = 1, col = c("black","red","blue"))
 
 ## Close off the call to png
 
